f78f6c980660a13f450a1fe8ab0a5603a7e07216
Hayden D’Souza <haydenfds@gmail.com>
2023-09-13 04:39:54 -0700

    Aesthetic edits to navbar + add context dir

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Nav = () => {
   return (
     <div className='min-w-full bg-green-600 p-2'>
-        <h1 className='text-2xl font-semibold'>Duplify</h1>
+        <h1 className='text-4xl font-semibold tracking-wider uppercase p-2 w-[20%] ml-0'>Duplify</h1>
     </div>
   )
 }
