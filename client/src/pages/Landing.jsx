00d32569301986dfa1481b2d890255909c9e2726
Hayden D’Souza <haydenfds@gmail.com>
2023-09-14 06:54:37 -0700

    More framer configs

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Button } from '@nextui-org/react'
 
 export const Landing = () => {
 
@@ -8,10 +9,14 @@ export const Landing = () => {
         visible: { opacity: 1 }, // Final state, fully visible
       };
 
+    const OauthRedirect = () => {
+      window.location.href='http://localhost:8000/authorize'
+    }
+
   return (
     <div className='min-h-[50rem] text-center flex flex-col justify-center my-auto'>
         <motion.h1 
-        className='font-bold text-7xl tracking-wider uppercase'
+        className='font-bold text-7xl tracking-widest uppercase'
         initial={{ y: '-1000%' }}
         animate={{ y: 0 }}
         transition={{ type: 'spring', duration: 2 }}
@@ -19,18 +24,20 @@ export const Landing = () => {
             DUPLIFY
         </motion.h1>
         <motion.h1 
-        className='font-medium text-3xl mt-6 text-center leading-relaxed'
+        className='font-medium text-3xl mt-6 text-center leading-relaxed px-4'
         initial={{ y: '-1000%' }}
         animate={{ y: 0 }}
         transition={{ type: 'spring', duration: 2 }}
         >
             Clone the contents of other Spotify playlists to a personal playlist!
         </motion.h1>
-        <motion.a initial="hidden" animate="visible" variants={fadeIn} transition={{delay: 1.5, duration: 1 }}
-        className='mt-10 p-4 cursor-pointer border-b-primaryGreen bg-primaryGreen w-[150px] mx-auto rounded-md text-white text-2xl hover:bg-primaryPurple font-semibold'
-        href='http://localhost:8000/authorize'
-        >
-            LOGIN</motion.a>
+        <motion.div className="mt-10" initial="hidden" animate="visible" variants={fadeIn} transition={{delay: 1.5, duration: 1}}>
+          <Button 
+          onClick={() => OauthRedirect()}
+          className='bg-primaryGreen text-white font-semibold hover:bg-primaryPurple rounded-xl text-xl w-[16rem] h-[4rem]'>
+            Login to Spotify
+          </Button>
+        </motion.div>  
     </div>
   )
 }
