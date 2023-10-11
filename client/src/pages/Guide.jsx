41c89149e4001f37186b68f9b6a4422d40995187
Hayden D’Souza <haydenfds@gmail.com>
2023-10-11 15:24:58 -0700

    Added slideshow component w/ basic styling

--- client/src/pages/Guide.jsx
+++ client/src/pages/Guide.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Nav } from '../components/Nav'
 import { motion } from 'framer-motion'
+import { Slideshow } from '../components/Slideshow'
+import { slides } from '../utils/slideshow'
 
 export const Guide = () => {
 
@@ -10,10 +12,11 @@ export const Guide = () => {
     initial={{ y: '-1000%', opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ type: 'tween', duration: 1.5 }}
-    className='text-center'>
+    className='text-center min-h-full'>
         <Nav />
           <div className='text-center mt-10'>
             <h1 className='text-3xl md:text-5xl font-semibold'>How to <h1 className='text-primaryGreen inline tracking-wider'>Duplify</h1></h1>
+            <Slideshow slides={slides}/>
           </div>
       </motion.div>
     </>
---
+++ client/src/utils/slideshow.js
@@ -0,0 +1,21 @@
+import Step1 from '../images/guide-step-1.png'
+import Step2 from '../images/guide-step-2.png'
+import Step3 from '../images/guide-step-3.png'
+
+export const slides = [
+    { 
+        url: Step1,
+        title: 'Step 1',
+        text: 'Find the playlist that you\'d like to clone. Click the more options button (3 dots).'
+    },
+    { 
+        url: Step2,
+        title: 'Step 2',
+        text: 'At the very bottom of the dropdown menu, click on Share. Choose \"Copy link to playlist\". The playlist link will be saved to your clipboard.',
+    },
+    { 
+        url: Step3,
+        title: 'Step 3',
+        text: 'Paste the URL and xxx.'
+    },
+  ]
