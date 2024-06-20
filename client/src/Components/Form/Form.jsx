141de0eacb275f819db8d5e8816c4aa8ebc0e17d
Hayden D’Souza <haydenfds@gmail.com>
2024-06-20 00:11:44 -0700

    Added spinner loading while playlist is being created

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,99 +1,97 @@
-import React, { useState } from 'react'
-import {RadioGroup, Radio, cn, Button} from "@nextui-org/react";
-import { motion } from 'framer-motion'
+import React, { useState } from 'react';
+import { RadioGroup, Radio, cn, Button } from "@nextui-org/react";
+import { motion } from 'framer-motion';
 import {
   validateToken,
   BACKEND_ENDPOINTS
-} from '../../Utils'
+} from '../../Utils';
 
-export const Form = ({playlist, user_id}) => {
-
-  const [playlistName, setPlaylistName] = useState('')
-  const [playlistDescription, setPlaylistDescription] = useState('')
-  const [playlistVisibility, setPlaylistVisibility] = useState('public')
+export const Form = ({ playlist, user_id, setIsPlaylistCreationLoading }) => {
+  const [playlistName, setPlaylistName] = useState('');
+  const [playlistDescription, setPlaylistDescription] = useState('');
+  const [playlistVisibility, setPlaylistVisibility] = useState('public');
 
   const handlePlaylistNameChange = (e) => {
-    setPlaylistName(e.target.value)
-  }
+    setPlaylistName(e.target.value);
+  };
 
   const handlePlaylistDescriptionChange = (e) => {
-    setPlaylistDescription(e.target.value)
-  }
-
+    setPlaylistDescription(e.target.value);
+  };
 
   const handleSubmit = async () => {
-
-    const token = validateToken()
-
+    const token = validateToken();
     if (!token) {
-      window.location.href = '/'
-
+      window.location.href = '/';
     } else {
-    const id = localStorage.getItem('duplify_playlist_id')
-    const url = BACKEND_ENDPOINTS.CREATE_PLAYLIST + `?token=${token}&id=${playlist.id}`
-    
-    const body = {
-      user_id,
-      playlistName, 
-      playlistDescription,
-      playlistVisibility: playlistVisibility == "public"? true: false,
-    }
+      setIsPlaylistCreationLoading(true); // Set loading to true
+      const id = localStorage.getItem('duplify_playlist_id');
+      const url = BACKEND_ENDPOINTS.CREATE_PLAYLIST + `?token=${token}&id=${playlist.id}`;
 
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      }, 
-      body: JSON.stringify(body)
-    }).then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })
-    }
-  }
+      const body = {
+        user_id,
+        playlistName,
+        playlistDescription,
+        playlistVisibility: playlistVisibility === "public" ? true : false,
+      };
 
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      }).then((res) => res.json())
+        .then((data) => {
+          setIsPlaylistCreationLoading(false); // Set loading to false when done
+          // Handle the response data
+        }).catch((error) => {
+          setIsPlaylistCreationLoading(false); // Set loading to false in case of an error
+          console.error('Error creating playlist:', error);
+        });
+    }
+  };
 
   return (
     <>
-        <motion.div 
-            initial={{ x: '-100%' }}
-            animate={{ x: 0 }}
-            transition={{ type: 'spring', duration: 1 }}
+      <motion.div
+        initial={{ x: '-100%' }}
+        animate={{ x: 0 }}
+        transition={{ type: 'spring', duration: 1 }}
         className='w-full bg-black border-4-black bg-opacity-60 rounded-lg px-4 py-6 text-white text-3xl'>
-            <div className='w-full font-semibold'>
-                <h2 className='inline'>Clone </h2>
-                <h2 className='text-primaryPurple inline'>{playlist?.name} </h2>
-                <h2 className='inline'>by </h2>
-                <h2 className='inline text-primaryGreen'>{playlist?.owner?.display_name} </h2>
-                <h2 className='inline bg-gradient-to-r from-purple-700 to-primaryPurple via-primaryGreen  text-transparent bg-clip-text'>({playlist?.tracks?.total} songs)</h2>
-            </div>
-            <div className='mt-6 flex flex-col gap-8 items-center text-black'>
-                <input type='text'
-                className=' text-lg rounded-md outline-none focus:outline-primaryGreen outline-4 p-2 w-3/5 font-semibold'
-                placeholder='Give your playlist a name' value={playlistName} onChange={(e) => handlePlaylistNameChange(e)}/>
-                <input type='text'
-                className='text-lg rounded-md outline-none focus:outline-primaryGreen outline-4 p-2 w-3/5 font-semibold'
-                placeholder='How about a description? (Optional)' value={playlistDescription} onChange={(e) => handlePlaylistDescriptionChange(e)}/>
-                <RadioGroup
-                    orientation="horizontal"
-                    label="Make your playlist public or private?"
-                    onChange={(e) => setPlaylistVisibility(e.target.value)}
-                    classNames={{
-                        label: cn("text-white text-lg text-left font-semibold"),
-                        base: cn("w-3/5")
-                    }}
-                    defaultValue="public"
-                    >
-                    <Radio value="public" classNames={{ label: cn("text-white mr-20 text-center font-medium"), control: cn("p-4 bg-primaryGreen border-none outline-none")}}>Public</Radio>
-                    <Radio value="private" classNames={{ label: cn("text-white mr-20 text-center font-medium"), control: cn("p-4 bg-primaryGreen border-none outline-none")}}>Private</Radio>
-                </RadioGroup>
-                <Button onClick={() => handleSubmit()}
-                className='bg-primaryGreen text-white font-semibold hover:bg-primaryPurple text-lg h-[3rem]'>
-                    Make me my playlist!
-                </Button>
-            </div>
-        </motion.div>
+        <div className='w-full font-semibold'>
+          <h2 className='inline'>Clone </h2>
+          <h2 className='text-primaryPurple inline'>{playlist?.name} </h2>
+          <h2 className='inline'>by </h2>
+          <h2 className='inline text-primaryGreen'>{playlist?.owner?.display_name} </h2>
+          <h2 className='inline bg-gradient-to-r from-purple-700 to-primaryPurple via-primaryGreen  text-transparent bg-clip-text'>({playlist?.tracks?.total} songs)</h2>
+        </div>
+        <div className='mt-6 flex flex-col gap-8 items-center text-black'>
+          <input type='text'
+            className=' text-lg rounded-md outline-none focus:outline-primaryGreen outline-4 p-2 w-3/5 font-semibold'
+            placeholder='Give your playlist a name' value={playlistName} onChange={(e) => handlePlaylistNameChange(e)} />
+          <input type='text'
+            className='text-lg rounded-md outline-none focus:outline-primaryGreen outline-4 p-2 w-3/5 font-semibold'
+            placeholder='How about a description? (Optional)' value={playlistDescription} onChange={(e) => handlePlaylistDescriptionChange(e)} />
+          <RadioGroup
+            orientation="horizontal"
+            label="Make your playlist public or private?"
+            onChange={(e) => setPlaylistVisibility(e.target.value)}
+            classNames={{
+              label: cn("text-white text-lg text-left font-semibold"),
+              base: cn("w-3/5")
+            }}
+            defaultValue="public"
+          >
+            <Radio value="public" classNames={{ label: cn("text-white mr-20 text-center font-medium"), control: cn("p-4 bg-primaryGreen border-none outline-none") }}>Public</Radio>
+            <Radio value="private" classNames={{ label: cn("text-white mr-20 text-center font-medium"), control: cn("p-4 bg-primaryGreen border-none outline-none") }}>Private</Radio>
+          </RadioGroup>
+          <Button onClick={() => handleSubmit()}
+            className='bg-primaryGreen text-white font-semibold hover:bg-primaryPurple text-lg h-[3rem]'>
+            Make me my playlist!
+          </Button>
+        </div>
+      </motion.div>
     </>
-  )
-}
-
+  );
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,186 +1,142 @@
-import React, {useState, useEffect } from 'react';
-import { cn, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from '@nextui-org/react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import {
-  validateToken, 
-  BACKEND_ENDPOINTS
-} from "../Utils";
+import { validateToken, BACKEND_ENDPOINTS } from "../Utils";
 import { Form } from '../Components';
 import { CustomButton } from '../Components';
+import { Spinner, cn} from '@nextui-org/react';
+
+const isValidSpotifyUrl = (url) => {
+  const spotifyUrlPattern = /^https:\/\/open\.spotify\.com\/playlist\/[a-zA-Z0-9]+(\?si=[a-zA-Z0-9]+)?$/;
+  return spotifyUrlPattern.test(url);
+};
 
 export const Home = () => {
-
-  const [searchInput, setSearchInput] = useState('')
-  const [user, setUser] = useState({})
-  const [playlist, setPlaylist] = useState({})
-  const [isSearchInputValid, setIsSearchInputValid] = useState(true)
-
-  const {isOpen, onOpen, onClose} = useDisclosure()
+  const [searchInput, setSearchInput] = useState('');
+  const [user, setUser] = useState({});
+  const [playlist, setPlaylist] = useState({});
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [isPlaylistCreationLoading, setIsPlaylistCreationLoading] = useState(false);
 
   const getUser = async () => {
-
-    const access_token = validateToken()
-
+    const access_token = validateToken();
     if (access_token) {
-
-      const url = BACKEND_ENDPOINTS.USER + `?access_token=${access_token}`
-      
+      const url = BACKEND_ENDPOINTS.USER + `?access_token=${access_token}`;
       await fetch(url, {
-        method: 'GET', 
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-        }, 
+        },
       }).then((res) => res.json()).then(data => {
-        setUser(data.user)
-      })
+        console.log(data.user);
+        setUser(data.user);
+      });
+    } else {
+      window.location.href = '/';
     }
-
-    else {
-      window.location.href = '/'
-    }
-  }
+  };
 
   useEffect(() => {
-    
-    const urlParams = new URLSearchParams(window.location.search)
-
+    const urlParams = new URLSearchParams(window.location.search);
     if (validateToken()) {
-
-      getUser()
-    }
-
-    else if (urlParams.get('access_token')) {
-
-      const token = urlParams.get('access_token')
-      const duplify_token = token
-      const expiration_time = new Date().getTime() + (59 * 60 * 1000)
+      getUser();
+    } else if (urlParams.get('access_token')) {
+      const token = urlParams.get('access_token');
+      const duplify_token = token;
+      const expiration_time = new Date().getTime() + (59 * 60 * 1000);
 
       const token_data = {
         token: duplify_token,
         expiration_time: expiration_time
-      }
+      };
 
-      localStorage.setItem('duplify_token', JSON.stringify(token_data))
-      getUser()
+      localStorage.setItem('duplify_token', JSON.stringify(token_data));
+      getUser();
+    } else {
+      window.location.href = '/';
     }
+  }, []);
 
-    else {
-      window.location.href = '/'
-    }
+  // useEffect(() => {
+  //   const checkClipboard = async () => {
+  //     try {
+  //       const text = await navigator.clipboard.readText();
+  //       if (isValidSpotifyUrl(text)) {
+  //         setSearchInput(text);
+  //       }
+  //     } catch (err) {
+  //       console.error('Failed to read clipboard contents: ', err);
+  //     }
+  //   };
 
+  //   const handleVisibilityChange = () => {
+  //     if (document.visibilityState === 'visible') {
+  //       setTimeout(checkClipboard, 100);  // Delay to ensure document is focused
+  //     }
+  //   };
 
-  }, [])
+  //   document.addEventListener('visibilitychange', handleVisibilityChange);
 
-// TODO: FIX THIS CLIPBOARD EFFECT
-// useEffect(() => {
-//     const checkClipboard = async () => {
-//       try {
-//         const text = await navigator.clipboard.readText();
-//         const spotifyUrlPattern = /^https:\/\/open\.spotify\.com\/playlist\/[a-zA-Z0-9]+/;
-//         if (spotifyUrlPattern.test(text)) {
-//           setSearchInput(text);
-//         }
-//       } catch (err) {
-//         console.error('Failed to read clipboard contents: ', err);
-//       }
-//     };
+  //   return () => {
+  //     document.removeEventListener('visibilitychange', handleVisibilityChange);
+  //   };
+  // }, []);
 
-//     const handleVisibilityChange = () => {
-//       if (document.visibilityState === 'visible') {
-//         setTimeout(checkClipboard, 100);  // Delay to ensure document is focused
-//       }
-//     };
-
-//     document.addEventListener('visibilitychange', handleVisibilityChange);
-
-//     return () => {
-//       document.removeEventListener('visibilitychange', handleVisibilityChange);
-//     };
-//   }, []);
-
-
-// TODO: Cleanup logic; clip whitespace at end. Fix response to invalid input
   const searchPlaylistById = async (input_url) => {
-
-    const spotifyUrlPattern = /^https:\/\/open\.spotify\.com\/playlist\/[a-zA-Z0-9]+(\?si=[a-zA-Z0-9]+)?$/;
-    if (!spotifyUrlPattern.test(input_url)) {
-      setIsSearchInputValid(false);
-      setSearchInput("");
+    if (!isValidSpotifyUrl(input_url)) {
+      setIsInvalid(true);
       return;
     }
 
-    setIsSearchInputValid(true);
+    const re = /playlist\/([^/]+)\?/;
+    const match = input_url.match(re);
 
-    const re = /playlist\/([^/]+)\?/
-
-    const match = input_url.match(re)
-
-    if (match && match[1])
-    {
-      const _id = match[1]
-      const token = validateToken()
-      console.log('token')
+    if (match && match[1]) {
+      const _id = match[1];
+      const token = validateToken();
       if (token) {
-
-        const url = BACKEND_ENDPOINTS.FETCH_PLAYLIST + `?token=${token}&pid=${_id}`
-        console.log(url)
-        await fetch(url).then((res) => res.json()).then(data => 
-          {
+        const url = BACKEND_ENDPOINTS.FETCH_PLAYLIST + `?token=${token}&pid=${_id}`;
+        await fetch(url).then((res) => res.json()).then(data => {
           setPlaylist(data.data);
           console.log('Fetched playlist: ' + _id);
-          localStorage.setItem('duplify_playlist_id', _id);
-        })
+          setIsInvalid(false);
+        });
       } else {
-        window.location.href = '/'
+        window.location.href = '/';
       }
     }
-  }
+  };
 
   return (
     <>
-      <motion.div 
+      <motion.div
         initial={{ y: '-1000%', opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ type: 'tween', duration: 1.5 }}
         className='text-center'>
 
         <h3 className='mt-10 text-xl md:text-3xl font-semibold w-full leading-relaxed'>
-            Hi, <span className='inline text-primaryGreen'>{user?.display_name}</span>. What playlists are we cloning today?
+          Hi, <span className='inline text-primaryGreen'>{user?.display_name}</span>. What playlists are we cloning today?
         </h3>
 
-        <div className='w-[100vw] mt-10 flex flex-nowrap gap-8 mx-auto justify-center relative'>
+        <div className='w-[100vw] mt-10 flex flex-nowrap gap-8 mx-auto justify-center'>
           <input type='text' placeholder='Enter a playlist URL'
-          value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
-          className={`outline-none focus:border-primaryGreen p-2 rounded-xl w-1/2 text-black text-md text-xl font-semibold border-3 border-transparent ml-0 text-ellipsis overflow-hidden whitespace-nowrap ${!isSearchInputValid ? 'border-red-500':'' }`}
+            value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
+            className='outline-none focus:border-primaryGreen p-2 rounded-xl w-1/2 text-black text-md text-xl font-semibold border-3 border-transparent ml-0 text-ellipsis overflow-hidden whitespace-nowrap'
           />
-          <CustomButton onClickEvent={() => searchPlaylistById(searchInput)} textContent="Search"/>
-          {/* { setIsSearchInputValid ? <></> : <p className='text-blue-300'>Please enter valid playlist URL. Eg. https://open.spotify.com/playlist/....</p>} */}
+          <CustomButton onClickEvent={() => searchPlaylistById(searchInput)} textContent="Search" styles='h-[1/2]' />
         </div>
+        {isInvalid && <p className="text-red-500">Please enter a valid Spotify playlist URL.</p>}
         <div className='mt-10 w-4/5 mx-auto'>
-          {Object.keys(playlist).length > 0 && <Form playlist={playlist} user_id={user.id}/>}
+          {Object.keys(playlist).length > 0 && <Form playlist={playlist} user_id={user.id} setIsPlaylistCreationLoading={setIsPlaylistCreationLoading} />}
         </div>
       </motion.div>
 
-{/* 
-      <Modal isOpen={isOpen} onClose={onClose} size="md" placement='center'
-      classNames={ {
-        base: cn("bg-gray-800"),
-        closeButton: "mt-2 hover:bg-primaryGreen/75 active:bg-transparent"
-      }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className='font-bold uppercase mx-auto w-full justify-center border-b-2 border-primaryGreen text-2xl'>UH-OH!</ModalHeader>
-              <ModalBody className='m-2 text-lg leading-relaxed font-normal'>
-                <p> 
-                Looks like you entered an invalid URL! Please make sure...
-                </p>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal> */}
+      {isPlaylistCreationLoading && (
+        <div className="fixed inset-0 flex flex-col gap-y-12 items-center justify-center bg-black bg-opacity-80 z-50">
+          <Spinner size="3xl" className="scale-[250%]" color="success"/>
+          <h2 className='text-white font-bold text-2xl'>Loading...</h2>
+        </div>
+      )}
     </>
-  )
+  );
 }
